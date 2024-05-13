1d6a221396859499bc2a67ff9a8f0ca35f1f3102
Mariano Abadie <mabadie@aplay.com.ar>
2024-05-13 08:14:50 -0300

    add rutas y controladores

--- controllers/entidad.controller.js
+++ controllers/entidad.controller.js
@@ -1,16 +1,84 @@
 //definición de lista de entidades
-const listaEntidades = [{
-    id: 1,
-    borrado: false
-    //... aqui van las demás propiedades
-}]
+const listaEntidades = [
+    {
+        id: 1, //lo proporciona el servidor
+        borrado: false,
+        //... aqui van las demás propiedades
+        titulo: "estudiar programación", //lo proporciona el front
+        descripcion: "Estudiar por la mañana tomando matecitos", //lo proporciona el front
+        hecho: false,
+    },
+    {
+        id: 2, //lo proporciona el servidor
+        borrado: false,
+        //... aqui van las demás propiedades
+        titulo: "estudiar programación", //lo proporciona el front
+        descripcion: "Estudiar por la mañana tomando matecitos", //lo proporciona el front
+        hecho: false,
+    },
+    {
+        id: 3, //lo proporciona el servidor
+        borrado: false,
+        //... aqui van las demás propiedades
+        titulo: "estudiar programación", //lo proporciona el front
+        descripcion: "Estudiar por la mañana tomando matecitos", //lo proporciona el front
+        hecho: false,
+    }
+];
 
 //funcion controladora que devuelve la lista de entidades
 exports.obtenerLista = (req, res) => {
     try {
-        return res.status(200).send({success: true, data: listaEntidades})
+        return res.status(200).send({ success: true, data: listaEntidades });
     } catch (error) {
-        console.log("[obtenerLista] Error fatal: ",error)
-        return res.status(500).send({success: false, message: 'Error en el servidor'})
+        console.log("[obtenerLista] Error fatal: ", error);
+        return res.status(500).send({ success: false, message: "Error en el servidor" });
     }
-}
+};
+
+exports.crearTarea = (request, response) => {
+    //aqui va la lógica de agregar la entidad al array (listaEntidades)
+    //obtener datos del body - OK
+    //creen un objeto en base a esos datos - OK
+    //guardar el objeto en el array (listaEntidades) - OK
+
+    try {
+        const { titulo, descripcion } = request.body
+
+        if(!titulo || !descripcion){
+            return response.status(400).send({ success: false, message: 'Faltan datos'})
+        }
+
+        const id = listaEntidades.length + 1
+
+        const nuevaTarea = {
+            id: id,
+            borrado: false,
+            titulo: titulo,
+            descripcion: descripcion,
+            hecho: false
+        }
+
+        listaEntidades.push(nuevaTarea)
+
+        return response.status(200).send("tarea creada!");
+    } catch (error) {
+        console.log("error servidor: ",error)
+        return response.status(500).send({ success: false, message: "Error en el servidor" });
+    }
+};
+
+exports.eliminarTarea = (request, response) => {
+
+    const { id } = request.params
+
+    listaEntidades.forEach(entidad => {
+        if(entidad.id == id){
+            entidad.borrado = true
+        }
+    })
+
+    return response.status(200).send(`tarea con id: ${id}, eliminada`);
+};
+
+//TODO: ACTUALIZAR TAREA
--- routes/entidad.route.js
+++ routes/entidad.route.js
@@ -1,8 +1,10 @@
 const express = require('express') //importo express
-const { obtenerLista } = require('../controllers/entidad.controller') //importo los controladores de la ruta
+const { obtenerLista, crearTarea, eliminarTarea } = require('../controllers/entidad.controller') //importo los controladores de la ruta
 const router = express.Router()
 
 //aqui definiriamos todas las rutas de mi entidad
 router.get('/lista', obtenerLista)
+router.post('/crear', crearTarea)
+router.patch('/eliminar/:id', eliminarTarea)
 
 module.exports = router //exporto el router
